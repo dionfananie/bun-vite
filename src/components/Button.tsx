e34bd69ff8238743ac56dda2c7e57b1339977d80
dion fananie <dionfananie@gmail.com>
2023-09-29 00:19:52 +0700

    feat(chore): playing artifacts

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ const Button = ({ text }: { text: string }) => {
       type="button"
       className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
     >
-      {text} Click this
+      {text} Click this Button
     </button>
   );
 };
