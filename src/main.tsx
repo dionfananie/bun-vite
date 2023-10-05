b2dcd6af8e46c3fff7ceab6b4db799bac270dead
dion fananie <dionfananie@gmail.com>
2023-10-05 20:26:31 +0700

    add example nih

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById(
-    "root",
-  ) as HTMLElement,
-).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+// hello this is example code 2 heheheheheheh
